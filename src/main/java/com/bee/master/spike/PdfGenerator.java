e3cacdce0d68751ac2d08672bca11e68088bd21c
fantasy130 <fantasy850810@163.com>
2020-04-23 15:38:19 +0800

    Adjust itext to the MPL license version

--- src/main/java/com/bee/master/spike/PdfGenerator.java
+++ src/main/java/com/bee/master/spike/PdfGenerator.java
@@ -1,9 +1,9 @@
 package com.bee.master.spike;
 
-import com.itextpdf.text.*;
-import com.itextpdf.text.pdf.BaseFont;
-import com.itextpdf.text.pdf.PdfContentByte;
-import com.itextpdf.text.pdf.PdfWriter;
+import com.lowagie.text.*;
+import com.lowagie.text.pdf.BaseFont;
+import com.lowagie.text.pdf.PdfContentByte;
+import com.lowagie.text.pdf.PdfWriter;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -14,16 +14,16 @@ import java.nio.file.Paths;
 public class PdfGenerator {
     public static byte[] generate() throws Exception {
 
-
         Document document = new Document(PageSize.A4.rotate(), 25, 25, 30, 30);
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         PdfWriter writer = PdfWriter.getInstance(document, baos);
 
         document.open();
-        document.add(background());
         setText(writer, "Winfield Feng", 420, 370);
         setText(writer, "Bee Master Course", 420, 240);
         setText(writer, "2020/4/22", 270, 120);
+
+        document.add(background());
         document.close();
 
         return baos.toByteArray();
@@ -45,3 +45,4 @@ public class PdfGenerator {
         cb.endText();
     }
 }
+
