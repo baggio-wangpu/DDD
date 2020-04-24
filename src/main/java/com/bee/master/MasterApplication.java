ec0204dee271aa1dd75164bfaaa4b027dbf40152
fantasy130 <fantasy850810@163.com>
2020-04-24 10:28:25 +0800

    Transfer login request to gaia business

--- src/main/java/com/bee/master/MasterApplication.java
+++ src/main/java/com/bee/master/MasterApplication.java
@@ -3,9 +3,11 @@ package com.bee.master;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableConfigurationProperties
+@EnableFeignClients
 public class MasterApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/bee/master/adapter/restful/controller/TokenController.java
+++ src/main/java/com/bee/master/adapter/restful/controller/TokenController.java
@@ -2,12 +2,11 @@ package com.bee.master.adapter.restful.controller;
 
 import com.bee.master.adapter.client.TokenClient;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
+import javax.annotation.Resource;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -16,7 +15,7 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 @RequestMapping("/tokens")
 public class TokenController {
 
-    @Autowired
+    @Resource
     TokenClient tokenClient;
 
     @PostMapping(produces = APPLICATION_JSON_VALUE)
