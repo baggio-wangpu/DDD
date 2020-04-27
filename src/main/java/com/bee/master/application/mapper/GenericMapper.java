0715094f1dbb2632c67275967c74cee8907fcc46
陈洁 <chenyu530324@163.com>
2020-04-27 11:55:23 +0800

    added drop useless tables sql script

--- src/main/java/com/bee/master/application/mapper/GenericMapper.java
+++ src/main/java/com/bee/master/application/mapper/GenericMapper.java
@@ -1,10 +1,8 @@
 package com.bee.master.application.mapper;
 
-import com.bee.master.adapter.jpa.entity.RolePO;
-import com.bee.master.application.vo.RoleVO;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface GenericMapper {
-    RoleVO poToVO(RolePO rolePO);
+
 }
--- src/test/resources/sqls/insert_roles_data.sql
+++
@@ -1,2 +0,0 @@
-delete from roles where id = 1;
-insert into roles(id, title, description) values ( 1, 'testTitle', 'testDescription' );
