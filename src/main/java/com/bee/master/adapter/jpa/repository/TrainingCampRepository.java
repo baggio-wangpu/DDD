cc794a1e4027bcc195bcf53990f92b2595985239
Chen Liang <>
2020-04-27 16:08:33 +0800

    fix build

--- src/main/java/com/bee/master/adapter/jpa/repository/TrainingCampRepository.java
+++ src/main/java/com/bee/master/adapter/jpa/repository/TrainingCampRepository.java
@@ -11,6 +11,10 @@ import java.util.List;
 @Repository
 public interface TrainingCampRepository extends JpaRepository<TrainingCampPO, String> {
 
-    @Query(value = "select c from bm_training_camp_teacher_mapping as tu left join bm_training_camp as c on c.id = tu.training_camp_id where tu.teacher_id = :userId and c.status = 'ACTIVE' ORDER BY c.lastModifiedTime DESC ")
+    @Query(value = "select c from bm_training_camp_teacher_mapping as tu " +
+            "left join bm_training_camp as c on c.id = tu.training_camp_id " +
+            "where tu.teacher_id = :userId and c.status = 'ACTIVE' " +
+            "ORDER BY c.lastModifiedTime DESC",
+            nativeQuery = true)
     List<TrainingCampPO> findTrainingCampsByUserId(@Param("userId") String userId);
 }
