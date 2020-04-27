89f2f9edde492657c95681b1dd9623e9627ee448
fantasy130 <fantasy850810@163.com>
2020-04-27 14:17:16 +0800

    Add CampVO

--- src/main/java/com/bee/master/application/vo/CampVO.java
+++ src/main/java/com/bee/master/application/vo/CampVO.java
@@ -1,4 +1,33 @@
 package com.bee.master.application.vo;
 
+import lombok.*;
+
+import java.time.Instant;
+import java.util.List;
+
+import static java.util.Objects.isNull;
+
+@Setter
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CampVO {
+
+    private String id;
+
+    private String name;
+
+    private String clientName;
+
+    private Instant startDateTime;
+
+    private Instant endDateTime;
+
+    private String status;
+
+    private Instant createdTime;
+
+    private Instant lastModifiedTime;
+
 }
