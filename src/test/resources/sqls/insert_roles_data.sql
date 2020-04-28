7d2197ed393a7b8aec5548708b9b7561a251e0c7
fantasy130 <fantasy850810@163.com>
2020-04-28 12:16:06 +0800

    Fix fail test

--- src/test/resources/sqls/insert_roles_data.sql
+++ src/test/resources/sqls/insert_roles_data.sql
@@ -1,5 +1,5 @@
 delete from bm_teacher where id = 'teach@email.com';
-INSERT INTO bm_teacher values ('teach@email.com');
+INSERT INTO bm_teacher(id) values ('teach@email.com');
 
 delete from bm_student where id = 'student@email.com';
 INSERT INTO bm_student values ('student@email.com');
@@ -8,4 +8,4 @@ delete from bm_student where id = 'teachstudent@email.com';
 INSERT INTO bm_student values ('teachstudent@email.com');
 
 delete from bm_teacher where id = 'teachstudent@email.com';
-INSERT INTO bm_teacher values ('teachstudent@email.com');
+INSERT INTO bm_teacher(id) values ('teachstudent@email.com');
